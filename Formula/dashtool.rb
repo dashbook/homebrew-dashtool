b21800b7d22dd0ef6dd4a49114f30f643b9c5746
Jan Kaul <jankaul@mailbox.org>
2024-03-07 18:24:45 +0100

    update hash

--- Formula/dashtool.rb
+++ Formula/dashtool.rb
@@ -2,7 +2,7 @@ class Dashtool < Formula
   desc "Lakehouse build tool for Kubernetes"
   homepage "https://github.com/dashbook/dashtool"
   url "https://github.com/dashbook/dashtool/releases/download/0.1.0/dashtool_0.1.0_x86_64-apple-darwin.tar.gz"
-  sha256 "b874af85c29f6d09f3452409a97d595c01621dc8a1940f6474c2de35cd2b08cc"
+  sha256 "da525deff2eb40aa3738af2bff76c2f3997a103a85243e380de80ab051a9d8f8"
   version "0.1.0"
 
   def install
